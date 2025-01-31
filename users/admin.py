ac537215a0e7fc3306e68f2534c1185d97de7ebc
maciejChmielewski <maciej.chmielewski03@gmail.com>
2025-01-31 13:42:47 +0100

    test

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Dodatkowe informacje', {'fields': ('role', 'phone_number', 'address',
+                                             'vip_bonus')}),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
