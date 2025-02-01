7bb920bd1137294c853e0df9d1fdd81a7f96d28b
Angelika Grudzien <angelagrudzien80@gmail.com>
2025-02-01 11:08:26 +0100

    naprawa bledow

--- sklep_projekt/urls.py
+++ sklep_projekt/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
-    path('', include('sklep_projekt/views.home')),
+    path('', views.home),
 
 ]
