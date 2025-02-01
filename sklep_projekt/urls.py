1c2c894cee330e351448d25afbd0516e49ee9c8c
Angelika Grudzien <angelagrudzien80@gmail.com>
2025-02-01 11:16:36 +0100

    Naprawa strony gównej

--- sklep_projekt/urls.py
+++ sklep_projekt/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
-    path('', views.home),
-
+    path('', home)
 ]
